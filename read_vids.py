8df737dbf212c7f5edf6fb3866d6b1ae8e1e3462
balakg <balakg@mit.edu>
2018-06-06 16:14:18 -0400

    reorganized

--- read_vids.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import glob
-
-base_data_dir = '/afs/csail.mit.edu/u/b/balakg/pose/datasets/golfswinghd/frames/*' 
-vid_names = glob.glob(base_data_dir)
-
-for i in vid_names:
-	print i
